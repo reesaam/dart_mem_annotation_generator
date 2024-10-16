79423bdc18db908fe6767fa37abfd80e55fbc883
Resam Taghipour <resam.t@gmail.com>
2024-10-16 18:58:29 +0330

    First Successful Generation

--- annotation/example/lib/resources/classes/sample_class.dart
+++ annotation/example/lib/resources/classes/sample_class.dart
@@ -6,16 +6,6 @@ import 'sample_inner_class.dart';
 part 'sample_class.mem.dart';
 part 'sample_class.mem.g.dart';
 
-// // @Freezed(toJson: false, fromJson: false)
-// @Mem.entity()
-// class SampleEntity with _$SampleEntity {
-//   const factory SampleEntity({
-//     final String? name,
-//     final String? description,
-//     final SampleInnerEntity? innerModel,
-//   }) = _SampleEntity;
-// }
-
 @Mem()
 class Sample {
   const Sample({this.name, this.description, this.innerClass});
