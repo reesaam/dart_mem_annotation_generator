3aa175923d0f39c8ab110341ff9a4e54502348bc
Resam Taghipour <resam.t@gmail.com>
2024-10-19 18:07:31 +0330

    Annotation

--- annotation/lib/src/base.dart
+++ annotation/lib/src/base.dart
@@ -4,7 +4,7 @@ library;
 /// This is the Base Annotation Class which provides Types and Options for the Annotation
 ///
 /// The Annotation has several Types, but it couldn't use by itself, it should use by one of the types
-/// Every [type] has it's options which provides some features of each type, options are specifically recognized by the type
+/// Every [typeString] has it's options which provides some features of each type, options are specifically recognized by the type
 ///
 /// Types are known as a enum, but users will see them as a constructor and this is the only way of using this Annotation
 ///
@@ -15,10 +15,11 @@ import 'enums.dart';
 class Mem {
   final AnnotationTypes? type;
   final Type? as;
+  final bool? withFreezed;
 
-  const Mem({this.as}) : type = AnnotationTypes.model;
-  const Mem.model({this.as}) : type = AnnotationTypes.model;
-  const Mem.entity({this.as}) : type = AnnotationTypes.entity;
-  const Mem.json({this.as}) : type = AnnotationTypes.json;
-  const Mem.api({this.as}) : type = AnnotationTypes.api;
+  const Mem.all({this.as, this.withFreezed}) : type = AnnotationTypes.all;
+  const Mem.model({this.as, this.withFreezed}) : type = AnnotationTypes.model;
+  const Mem.entity({this.as, this.withFreezed}) : type = AnnotationTypes.entity;
+  const Mem.json({this.as, this.withFreezed}) : type = AnnotationTypes.json;
+  const Mem.api({this.as, this.withFreezed}) : type = AnnotationTypes.api;
 }
